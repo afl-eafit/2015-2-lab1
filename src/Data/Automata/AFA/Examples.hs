62ed4f6c6b37608ccd2f33e943cc1e5f90d7bdd5
agomezl <alegomez544@gmail.com>
2015-08-22 11:12:00 -0500

    more examples added

--- src/Data/Automata/AFA/Examples.hs
+++ src/Data/Automata/AFA/Examples.hs
@@ -27,3 +27,15 @@ m2 = AFA  (singleton $ SE Q0) empty (\_ _ -> empty) (SE Q0) empty
 
 m3 :: AFA StateM SymbolM
 m3 = AFA (singleton $ SE Q0)  empty (\_ _ -> empty) (SE Q0) (singleton $ SE Q0)
+
+m4 :: AFA Int Int
+m4 AFA (fromList [SE 0,SE 1,SE 2,SE 3])
+       (fromList [0..])
+       deltaM
+       (singleton  (SE 3))
+    where
+      deltaM :: S Int -> Int -> Set (S Int)
+      deltaM (SE 0) 0 = fromList [SE 0, SE 1]
+      deltaM (SE 1) 1 = fromList [SE 1, SE 2]
+      deltaM (SE 2) 2 = fromList [SE 2, SE 3]
+      deltaM _      _ = empty
