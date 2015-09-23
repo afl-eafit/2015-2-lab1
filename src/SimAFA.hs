77a8c3730931ff51273628e53001dbbdcfcc7028
agomezl <alegomez544@gmail.com>
2015-09-23 09:43:57 -0500

    ghc 7.8.4 nonsense fixed

--- src/SimAFA.hs
+++ src/SimAFA.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE CPP #-}
 --------------------------------------------------------------------------------
 -- File   : SimAFA
 -- Author : Alejandro Gómez Londoño
@@ -13,13 +14,18 @@ module SimAFA where
 
 import Data.Automata.AFA
 import Data.Automata.AFA.Examples
-import Data.Set      (member)
+import Data.Set       (member)
+import Data.Foldable  (all,any,Foldable,foldr)
+import Prelude hiding (all,any,null,foldr)
 
 accepts ∷ Ord state ⇒ AFA state symbol → [symbol] → Bool
 accepts x σ = acc x σ (initial x)
+
+null' ∷  Foldable t ⇒ t a -> Bool
+null' = foldr (\_ _ -> False) True
 
 acc ∷ Ord state ⇒ AFA state symbol → [symbol] → S state →  Bool
 acc x []    ρ        = member ρ (finals x)
 acc x (a:σ) ρ@(SE s) = let ω = (delta x ρ a) in any (acc x σ)  ω
 acc x (a:σ) ρ@(SA s) = let ω = (delta x ρ a) in all (acc x σ)  ω
-                                             && (not . null $  ω)
+                                             && (not . null' $  ω)
